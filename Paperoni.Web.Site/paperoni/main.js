2ba99424a1938605dff59488a9060bb49df5b163
pohnee <jt_pohnee39@yahoo.com>
2016-05-31 17:44:47 +0800

    Initial blank web site

---
+++ Paperoni.Web.Site/paperoni/main.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('paperoni')
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+
+})();
